e5320936264e822487480fe286c646394694da14
khuzan <khuzan557@gmail.com>
2024-03-23 21:37:40 +0800

    added project and testimonial component

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,43 @@
+
+const SocialMediaIcons = () => {
+    return (
+        <div className="flex justify-center md:justify-start my-10 gap-7">
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="#"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img src="../assets/linkedin.png" alt="linkedin-link" />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="#"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img src="../assets/twitter.png" alt="twitter-link" />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="#"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img src="../assets/facebook.png" alt="facebook-link" />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="#"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img src="../assets/instagram.png" alt="instagram-link" />
+            </a>
+        </div>
+    )
+
+}
+
+
+export default SocialMediaIcons;
